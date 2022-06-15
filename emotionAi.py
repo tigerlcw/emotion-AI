2fe6bb51a4571f69198ffd7e2708075c4f4a88e5
LINKER <temp2temp@daum.net>
2022-06-15 11:23:52 +0900

    lib setting

--- emotionAi.py
+++ emotionAi.py
@@ -1,1 +1,27 @@
-# 프로젝트 설정
+# 프로젝트에 필요한 라이브러리 셋팅
+# Tensorflow / keras API 활용
+import pandas as pd
+import numpy as np
+import os
+import PIL  # 이미지 처리 도움 라이브러리
+import seaborn as sns  # 데이터 시각화
+import pickle
+from PIL import *
+import cv2
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, LearningRateScheduler
+from IPython.display import display
+from tensorflow.python.keras import *
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers, optimizers
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.layers import *
+from tensorflow.keras import backend as K
+from keras import optimizers
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
